b5c225ae0ed72160a8f5bf2274eb892443238f13
Jinho Song <sth4881@naver.com>
2021-02-03 19:53:56 +0900

    [Update] flaskServer.py 및 html 파일 내용 업데이트

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import datetime
 
 from os import listdir, makedirs
 from os.path import isdir, isfile, join
@@ -7,7 +8,7 @@ from os.path import isdir, isfile, join
 # 사진을 200x200 사이즈로 crop 후에 회색 바탕으로 바꿔서 저장
 cropped_dirs = 'faces/cropped/'
 # 회원과 외부인을 가리지 않고 방문한 사람을 모두 촬영해서 웹으로 전송
-pictured_dirs = 'faces/pictured/'
+img_dirs = 'static/img/'
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
@@ -84,6 +85,10 @@ def face_detector(img):
     # 얼굴을 인식하지 못하면 None 리턴(안 써주면 오류 발생)
     if faces is ():
         return None
+        
+    # current = datetime.datetime.now()
+    # current_datetime = current.strftime('%Y-%m-%d %H:%M:%S')
+    # cv2.imwrite(img_dirs+current_datetime+'.jpg', img) # 얼굴을 인식하면 jpg 파일로 저장
 
     for(x, y, w, h) in faces:
         #cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
@@ -98,7 +103,6 @@ def face_identification(models):
     if cap.isOpened():
         cv2.namedWindow("Face Identification", cv2.WINDOW_AUTOSIZE)
 
-        count = 1
         while cv2.getWindowProperty("Face Identification", 0) >= 0:
             ret, img = cap.read()
             # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -112,9 +116,8 @@ def face_identification(models):
 
             # 카메라를 통해서 읽어온 이미지를 얼굴로 인식할 경우
             if face_detector(img) is not None:
-                cv2.imwrite(pictured_dirs+str(count)+'.jpg', img) # 얼굴을 인식하면 jpg 파일로 저장
                 face = cv2.resize(face_detector(img), (200, 200)) # 얼굴 사이즈에 맞게 200x200 사이즈로 이미지 crop
-                gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+                gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY) # crop한 이미지를 회색 배경으로 바꿔서 저장
 
                 # 학습한 모델로 예측 시도
                 min_score = 999
@@ -131,12 +134,15 @@ def face_identification(models):
                     display_string = str(confidence)+'% Confidence it is '+min_score_name
                 cv2.putText(img, display_string, (100,120), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
 
-                # confidence가 80이상인 경우 동일 인물로 인식하여 User 이름 표시
+                # confidence에 따라서 등록된 사용자인지 외부인인지 판별하여 이미지를 저장
+                current = datetime.datetime.now()
+                current_datetime = current.strftime('%Y-%m-%d %H:%M:%S ')
                 if confidence >= 80:
-                    cv2.putText(img, "User: "+min_score_name, (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                    cv2.putText(img, 'User:'+min_score_name, (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                    cv2.imwrite(img_dirs+current_datetime+'User:'+min_score_name+'.jpg', img) # 등록된 사용자로 인식될 경우에 대한 이미지 파일 저장
                 else:
-                    cv2.putText(img, "Unknown User", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-                count+=1
+                    cv2.putText(img, 'Unknown', (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+                    cv2.imwrite(img_dirs+current_datetime+'Unknown.jpg', img) # 외부인으로 인식될 경우에 대한 이미지 파일 저장
 
             cv2.imshow("Face Identification", img)
             keyCode = cv2.waitKey(1) & 0xFF
--- src/flaskServer.py
+++ src/flaskServer.py
@@ -1,13 +1,11 @@
-import face_detect
-import cv2
+import os, sys
 
-from flask import Flask, request, redirect, render_template, url_for
-
+from flask import Flask, request, redirect, render_template, send_file
 
 app = Flask(__name__) # Flask Class의 인스턴스 생성
 
 @app.route('/') # Flask에게 어떤 URL이 작성한 함수를 실행시킬지 알려줌
-@app.route('/index/')
+@app.route('/index')
 def index(): # 브라우저에 보여줄 메시지를 리턴하는 함수
     return render_template('index.html') # 단순히 메세지를 리턴하지 않고 template(html)로 이동시킴
 
@@ -18,6 +16,18 @@ def test():
     elif request.method == 'POST':
         value = request.form['aaa'] # test.html에서 input name이 aaa인 정보를 받아옴
         return value # 받아온 정보는 value에 담기게 되고 그것을 최종적으로 반환
+
+@app.route('/monitor/', methods=['GET', 'POST'])
+def monitor():
+    if request.method == 'GET':
+        path = 'static/pictured/'
+        files = os.listdir(path)
+        files.sort() # 파일을 오름차순으로 정렬
+        files.reverse() # 파일을 내림차순으로 정렬
+        return render_template('monitor.html', path='../'+path, files=files)
+    elif request.method == 'POST':
+        img = request.form
+        return "pictured/2021-02-02_11:58:11_User:Jinho.jpg"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True) # 로컬서버로 실행, app.run(host='0.0.0.0')으로 모든 OS에게 접근가능하도록 설정
