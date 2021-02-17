696baeece36d52bb6c0c85599e3efa3ba05bd1e8
sth4881 <sth4881@naver.com>
2021-02-17 22:40:27 +0900

    [Update] 서버 UI 적용

--- src/flaskServer.py
+++ src/flaskServer.py
@@ -1,8 +1,8 @@
 import os, sys, json
 
 from flask import Flask, request, redirect, render_template, jsonify, make_response
-from app import gstreamer_pipeline, train_model, train_models, face_detector, face_identification
 from face_save import gstreamer_pipeline, face_detector, face_save
+from face_identification import gstreamer_pipeline, train_model, train_models, face_detector, face_identification
 
 app = Flask(__name__) # Flask Class의 인스턴스 생성
 
@@ -11,13 +11,13 @@ app = Flask(__name__) # Flask Class의 인스턴스 생성
 def index(): # 브라우저에 보여줄 메시지를 리턴하는 함수
     return render_template('index.html') # 단순히 메세지를 리턴하지 않고 template(html)로 이동시킴
 
-@app.route('/test/', methods=['GET', 'POST']) # POST 형식으로 선언
-def test():
-    if request.method == 'GET':
-        return render_template('test.html')
-    elif request.method == 'POST':
-        value = request.form['aaa'] # test.html에서 input name이 aaa인 정보를 받아옴
-        return value # 받아온 정보는 value에 담기게 되고 그것을 최종적으로 반환
+# @app.route('/test/', methods=['GET', 'POST']) # POST 형식으로 선언
+# def test():
+#     if request.method == 'GET':
+#         return render_template('test.html')
+#     elif request.method == 'POST':
+#         value = request.form['aaa'] # test.html에서 input name이 aaa인 정보를 받아옴
+#         return value # 받아온 정보는 value에 담기게 되고 그것을 최종적으로 반환
 
 @app.route('/crop/', methods=['GET', 'POST'])
 def crop():
